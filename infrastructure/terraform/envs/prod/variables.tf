4b7a7e07b8db5273047ecd4c48430c0e11e9f12c
jakkalilokesh <jakkalilokesh@gmail.com>
2026-06-21 16:59:23 +0530

    feat(eks): change node instance type to t3.small

--- infrastructure/terraform/envs/prod/variables.tf
+++ infrastructure/terraform/envs/prod/variables.tf
@@ -42,7 +42,7 @@ variable "k8s_version" {
 
 variable "node_instance_type" {
   type        = string
-  default     = "t3.micro"
+  default     = "t3.small"
   description = "Instance type for EKS worker nodes"
 }
 
