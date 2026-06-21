c6d4bea6004ccbb6a0e9e428120189708d4bc41a
jakkalilokesh <jakkalilokesh@gmail.com>
2026-06-21 15:20:25 +0530

    chore(terraform): configure S3 remote state backend

--- infrastructure/terraform/envs/prod/backend.tf
+++ infrastructure/terraform/envs/prod/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     # Replace these placeholders with the actual names output by the bootstrap step:
-    bucket         = "mediguide-tf-state-mediguide"
+    bucket         = "mediguide-tf-state-tarui0jx"
     key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "ap-southeast-1"
     dynamodb_table = "mediguide-tf-locks"
     encrypt        = true
   }
